8fde02afb1745df9e203605249ae98ed10bc4f41
Faizan Ahmad <faizanahd289@gmail.com>
2026-08-13 16:37:35 +0500

    fix console issue3

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,6 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
       { url: '/icon.png', type: 'image/png' },
     ],
     apple: [
